c235ed47cee036d692357c3ca297a0fe1f46da4d
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2021-01-28 07:58:02 +0200

    Code refactoring and added Error Handling cases in code. Unit test need to be done.

--- friends/src/main/java/com/wiredbrain/friends/model/Friend.java
+++ friends/src/main/java/com/wiredbrain/friends/model/Friend.java
@@ -1,52 +1,21 @@
 package com.wiredbrain.friends.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Friend {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @JsonProperty("first-name")
+
+    @NotBlank
     private String firstName;
-    @JsonProperty("last-name")
     private String lastName;
-    int age;
-    @JsonIgnore
-    boolean married;
 
-    @JsonManagedReference
-    @OneToMany(mappedBy = "friend", cascade = CascadeType.ALL)
-    List<Address> addresses;
-
-    public List<Address> getAddresses() {
-        return addresses;
-    }
-
-    public void setAddresses(List<Address> addresses) {
-        this.addresses = addresses;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public boolean isMarried() {
-        return married;
-    }
-
-    public void setMarried(boolean married) {
-        this.married = married;
-    }
 
     public int getId() {
         return id;
