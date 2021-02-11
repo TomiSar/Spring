72234ddf4124c07792e82ef88d6fdf0186d4ea26
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2021-02-11 20:25:31 +0200

    Unit tests added and test passing with embedded MongoDB. Final step to verify the tests with real database.

--- airport-management/src/main/java/pluralsight/airportmanagement/AirportManagementApplication.java
+++ airport-management/src/main/java/pluralsight/airportmanagement/AirportManagementApplication.java
@@ -1,9 +1,7 @@
 package pluralsight.airportmanagement;
 
-import com.github.mongobee.Mongobee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
@@ -23,27 +21,5 @@ public class AirportManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AirportManagementApplication.class, args);
-    }
-
-    @Bean
-    public Mongobee mongobee() {
-        String mongoUri = environment
-                .getProperty("spring.data.mongodb.uri");
-        boolean migrationsEnabled = Boolean.parseBoolean(environment
-                .getProperty("app.db.migrations.enabled"));
-
-        Mongobee runner = new Mongobee(mongoUri);
-
-        runner.setEnabled(migrationsEnabled);
-
-        runner.setChangeLogsScanPackage("pluralsight.airportmanagement.db.migrations");
-
-        runner.setChangelogCollectionName("migrations");
-
-        runner.setLockCollectionName("migrations_lock");
-
-        runner.setMongoTemplate(this.mongoTemplate);
-
-        return runner;
     }
 }
--- airport-management/src/main/java/pluralsight/airportmanagement/domain/Airport.java
+++ airport-management/src/main/java/pluralsight/airportmanagement/domain/Airport.java
@@ -46,4 +46,3 @@ public class Airport {
         this.passengersServed = passengersServed;
     }
 }
-
